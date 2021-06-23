f256e6d98d14110ec42bd96633b28ee90204dfd7
Zhengkun Wang <zkwang@thoughtworks.com>
2021-06-24 00:23:07 +0800

    change error message for exceptions

--- src/main/kotlin/zkwang/excelk/exceptions/SheetNameAnnotationRequiredException.kt
+++ src/main/kotlin/zkwang/excelk/exceptions/SheetNameAnnotationRequiredException.kt
@@ -1,4 +1,4 @@
 package zkwang.excelk.exceptions
 
 class SheetNameAnnotationRequiredException(typeName: String) :
-    Throwable(message = "For $typeName, SheetName annotation is required")
+    Throwable(message = "For type $typeName, SheetName annotation is required")
